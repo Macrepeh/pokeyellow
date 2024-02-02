fc3a295df381c21f0eac47b2210cf927b59ae37f
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-01 23:13:52 -0500

    Update arbok.asm

--- data/pokemon/base_stats/arbok.asm
+++ data/pokemon/base_stats/arbok.asm
@@ -1,11 +1,11 @@
 	db DEX_ARBOK ; pokedex id
 
-	db  60,  85,  69,  80,  65
+	db  160,  75,  15,  40,  85
 	;   hp  atk  def  spd  spc
 
-	db POISON, POISON ; type
-	db 90 ; catch rate
-	db 147 ; base exp
+	db POISON, FIRE ; type
+	db 60 ; catch rate
+	db 130 ; base exp
 
 	INCBIN "gfx/pokemon/front/arbok.pic", 0, 1 ; sprite dimensions
 	dw ArbokPicFront, ArbokPicBack
