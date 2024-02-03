1d65b79405eb6f975e3749aec55d92394a8aaf92
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-02 23:49:51 -0500

    Update flareon.asm

--- data/pokemon/base_stats/flareon.asm
+++ data/pokemon/base_stats/flareon.asm
@@ -1,6 +1,6 @@
 	db DEX_FLAREON ; pokedex id
 
-	db  65, 130,  60,  65, 110
+	db  85, 140,  40,  75, 90
 	;   hp  atk  def  spd  spc
 
 	db FIRE, FIRE ; type
@@ -14,7 +14,7 @@
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   \
+	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   		   \
 	     RAGE,         MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
 	     FIRE_BLAST,   SWIFT,        SKULL_BASH,   REST,         SUBSTITUTE
 	; end
