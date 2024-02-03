3bf46e9d0a62879c7146f4e88ed946df048af3ab
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-02 23:55:38 -0500

    Update scyther.asm

--- data/pokemon/base_stats/scyther.asm
+++ data/pokemon/base_stats/scyther.asm
@@ -1,6 +1,6 @@
 	db DEX_SCYTHER ; pokedex id
 
-	db  70, 110,  80, 105,  55
+	db  70, 130,  90, 125,  20
 	;   hp  atk  def  spd  spc
 
 	db BUG, FLYING ; type
@@ -10,11 +10,11 @@
 	INCBIN "gfx/pokemon/front/scyther.pic", 0, 1 ; sprite dimensions
 	dw ScytherPicFront, ScytherPicBack
 
-	db QUICK_ATTACK, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db QUICK_ATTACK, PIN_MISSILE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm SWORDS_DANCE, TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   \
+	tmhm SWORDS_DANCE, TOXIC,        BODY_SLAM,    DOUBLE_EDGE,  HYPER_BEAM,   \
 	     RAGE,         MIMIC,        DOUBLE_TEAM,  BIDE,         SWIFT,        \
 	     SKULL_BASH,   REST,         SUBSTITUTE,   CUT
 	; end
