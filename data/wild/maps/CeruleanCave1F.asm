8e31de0f1b3062732fad3957f97cba79f265706b
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-12 01:40:40 -0500

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,15 +1,15 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 50, GOLBAT
 	db 55, GOLBAT
-	db 45, GRAVELER
-	db 55, GLOOM
-	db 55, WEEPINBELL
-	db 52, SANDSLASH
-	db 54, VENOMOTH
-	db 54, PARASECT
-	db 55, DITTO
-	db 60, DITTO
+	db 60, TAUROS
+	db 50, GRAVELER
+	db 60, GLOOM
+	db 60, WEEPINBELL
+	db 57, SANDSLASH
+	db 59, VENOMOTH
+	db 59, PARASECT
+	db 60, ALAKAZAM
+	db 65, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
