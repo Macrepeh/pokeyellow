5e96622700c9b674e8fb76c07d724286a3df72de
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-12 01:35:35 -0500

    Update VictoryRoad2F.asm

--- data/wild/maps/VictoryRoad2F.asm
+++ data/wild/maps/VictoryRoad2F.asm
@@ -1,15 +1,15 @@
 VictoryRoad2FWildMons:
-	def_grass_wildmons 10 ; encounter rate
-	db 31, GEODUDE
-	db 36, GEODUDE
-	db 41, GEODUDE
-	db 44, ZUBAT
-	db 39, GOLBAT
+	def_grass_wildmons 15 ; encounter rate
+	db 36, GRAVELER
+	db 41, GRAVELER
+	db 46, GRAVELER
+	db 49, GRAVELER
+	db 44, GOLBAT
+	db 49, GRAVELER
+	db 50, GRAVELER
+	db 52, GRAVELER
 	db 44, GRAVELER
-	db 45, ONIX
-	db 47, ONIX
-	db 39, MACHOKE
-	db 42, MACHOKE
+	db 47, GRAVELER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
