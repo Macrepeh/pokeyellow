99d5b8971ac7f6d6a9ad5e3bfe1ca3f66756c6cb
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-12 01:49:36 -0500

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,14 +1,14 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, NIDORAN_M
-	db 16, NIDORAN_F
-	db 18, RATTATA
+	db 26, NIDORAN_F
+	db 18, RATICATE
 	db 18, NIDORAN_M
 	db 18, NIDORAN_F
-	db 17, SPEAROW
+	db 17, BUTTERFREE
 	db 18, NIDORINO
 	db 18, NIDORINA
-	db 20, RATICATE
+	db 20, GASTLY
 	db 19, FEAROW
 	end_grass_wildmons
 
