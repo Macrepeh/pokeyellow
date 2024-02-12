803f2aa0095c4080f83556565c9b4ab74b7ea365
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-12 01:46:30 -0500

    Update Route12.asm

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,15 +1,15 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 25, ODDISH
-	db 25, BELLSPROUT
-	db 28, PIDGEY
-	db 28, PIDGEOTTO
-	db 27, ODDISH
-	db 27, BELLSPROUT
-	db 29, GLOOM
-	db 29, WEEPINBELL
-	db 26, FARFETCHD
-	db 31, FARFETCHD
+	db 26, ODDISH
+	db 26, BELLSPROUT
+	db 29, PIDGEY
+	db 29, PIDGEOTTO
+	db 28, PONYTA
+	db 28, BELLSPROUT
+	db 30, GLOOM
+	db 30, WEEPINBELL
+	db 27, FARFETCHD
+	db 32, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 3 ; encounter rate
@@ -22,5 +22,5 @@ Route12WildMons:
 	db 15, SLOWPOKE
 	db 15, SLOWPOKE
 	db 15, SLOWBRO
-	db 20, SLOWBRO
+	db 30, SLOWBRO
 	end_water_wildmons
