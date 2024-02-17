19836fa57cd71f84823a385a412d62d44af777d2
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-16 23:41:36 -0500

    Update charizard.asm

--- data/pokemon/base_stats/charizard.asm
+++ data/pokemon/base_stats/charizard.asm
@@ -1,11 +1,11 @@
 	db DEX_CHARIZARD ; pokedex id
 
-	db  78,  84,  78, 100,  85
+	db  73,  80,  73, 95,  80
 	;   hp  atk  def  spd  spc
 
 	db FIRE, FLYING ; type
 	db 45 ; catch rate
-	db 209 ; base exp
+	db 129 ; base exp
 
 	INCBIN "gfx/pokemon/front/charizard.pic", 0, 1 ; sprite dimensions
 	dw CharizardPicFront, CharizardPicBack
