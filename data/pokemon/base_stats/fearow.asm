80d706bb41e858741daed7c470df168bc37b9255
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-16 23:47:27 -0500

    Update fearow.asm

--- data/pokemon/base_stats/fearow.asm
+++ data/pokemon/base_stats/fearow.asm
@@ -1,16 +1,16 @@
 	db DEX_FEAROW ; pokedex id
 
-	db  65,  90,  65, 100,  61
+	db  62,  92,  69, 121,  76
 	;   hp  atk  def  spd  spc
 
 	db NORMAL, FLYING ; type
 	db 90 ; catch rate
-	db 162 ; base exp
+	db 150 ; base exp
 
 	INCBIN "gfx/pokemon/front/fearow.pic", 0, 1 ; sprite dimensions
 	dw FearowPicFront, FearowPicBack
 
-	db PECK, GROWL, LEER, NO_MOVE ; level 1 learnset
+	db PECK, GROWL, LEER, RAZOR_WIND ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
