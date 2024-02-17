778f2752134ff0bd7c2e04d9e0d05e92bcc88c52
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-16 23:49:39 -0500

    This thing sucks

--- data/pokemon/base_stats/golbat.asm
+++ data/pokemon/base_stats/golbat.asm
@@ -1,16 +1,16 @@
 	db DEX_GOLBAT ; pokedex id
 
-	db  75,  80,  70,  90,  75
+	db  70,  75,  65,  85,  70
 	;   hp  atk  def  spd  spc
 
 	db POISON, FLYING ; type
 	db 90 ; catch rate
-	db 171 ; base exp
+	db 130 ; base exp
 
 	INCBIN "gfx/pokemon/front/golbat.pic", 0, 1 ; sprite dimensions
 	dw GolbatPicFront, GolbatPicBack
 
-	db LEECH_LIFE, SCREECH, BITE, NO_MOVE ; level 1 learnset
+	db RAZOR_WIND, SCREECH, BITE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
