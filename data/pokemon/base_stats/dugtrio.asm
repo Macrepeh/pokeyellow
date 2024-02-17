9efe08a5ee52bccac18ac2bc1619c0846b37be28
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-16 23:44:19 -0500

    Update dugtrio.asm

--- data/pokemon/base_stats/dugtrio.asm
+++ data/pokemon/base_stats/dugtrio.asm
@@ -1,11 +1,11 @@
 	db DEX_DUGTRIO ; pokedex id
 
-	db  35,  80,  50, 120,  70
+	db  10,  100,  30, 130,  70
 	;   hp  atk  def  spd  spc
 
 	db GROUND, GROUND ; type
 	db 50 ; catch rate
-	db 153 ; base exp
+	db 140 ; base exp
 
 	INCBIN "gfx/pokemon/front/dugtrio.pic", 0, 1 ; sprite dimensions
 	dw DugtrioPicFront, DugtrioPicBack
