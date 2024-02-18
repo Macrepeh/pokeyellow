84863797e0b33d7155d1ff77798886d89a38f280
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-18 15:15:48 -0500

    Update MtMoon1F.asm

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -2,12 +2,10 @@ MtMoon1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db  9, ZUBAT
 	db  9, ZUBAT
-	db 10, GEODUDE
 	db  6, ZUBAT
 	db  7, ZUBAT
 	db 10, ZUBAT
 	db 10, GEODUDE
-	db 11, ZUBAT
 	db 12, SANDSHREW
 	db 11, CLEFAIRY
 	db 11, PARAS
