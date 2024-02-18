2300e45d1e82b139b3da855b1374a5d4ed407e04
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-18 15:14:58 -0500

    Update CeruleanCave2F.asm

--- data/wild/maps/CeruleanCave2F.asm
+++ data/wild/maps/CeruleanCave2F.asm
@@ -2,15 +2,14 @@ CeruleanCave2FWildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 57, GOLBAT
 	db 62, ARCANINE
-	db 55, GRAVELER
+	db 55, DUGTRIO
 	db 61, SANDSLASH
 	db 55, RHYHORN
 	db 65, DITTO
-	db 63, VENUSAUR
-	db 70, ALAKAZAM
-	db 63, WEEPINBELL
+	db 63, DITTO
+	db 70, CHANSEY
 	db 65, RHYDON
-	db 63, RHYDON
+	db 65, RHYDON
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
