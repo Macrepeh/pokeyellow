0868e2ec289580f4ea4d38086c828e761c381308
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-18 15:13:40 -0500

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,14 +1,14 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 60, MACHAMP
-	db 60, TAUROS
 	db 60, GOLEM
-	db 60, GLOOM
+	db 60, GENGAR
+	db 60, TAUROS
 	db 60, WEEPINBELL
 	db 57, SANDSLASH
-	db 60, GENGAR
-	db 59, PARASECT
 	db 60, ALAKAZAM
+	db 59, PARASECT
+	db 60, GLOOM
 	db 65, DITTO
 	end_grass_wildmons
 
