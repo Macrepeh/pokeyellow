ccd3cff3705c2cdd5273a072eaa1e3e1c3fce193
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-18 18:35:57 -0500

    Update psyduck.asm

--- data/pokemon/base_stats/psyduck.asm
+++ data/pokemon/base_stats/psyduck.asm
@@ -1,11 +1,11 @@
 	db DEX_PSYDUCK ; pokedex id
 
-	db  50,  52,  48,  55,  50
+	db  50,  50,  50,  50,  50
 	;   hp  atk  def  spd  spc
 
 	db WATER, WATER ; type
-	db 190 ; catch rate
-	db 80 ; base exp
+	db 175 ; catch rate
+	db 75 ; base exp
 
 	INCBIN "gfx/pokemon/front/psyduck.pic", 0, 1 ; sprite dimensions
 	dw PsyduckPicFront, PsyduckPicBack
