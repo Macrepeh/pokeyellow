8d7d878cef3e33a5b59f3b31569347bd18eec920
Macrepeh <63179588+Macrepeh@users.noreply.github.com>
2024-02-18 19:16:30 -0500

    Update seaking.asm

--- data/pokemon/base_stats/seaking.asm
+++ data/pokemon/base_stats/seaking.asm
@@ -1,11 +1,11 @@
 	db DEX_SEAKING ; pokedex id
 
-	db  80,  92,  65,  68,  80
+	db  76,  98,  50,  72,  88
 	;   hp  atk  def  spd  spc
 
 	db WATER, WATER ; type
-	db 60 ; catch rate
-	db 170 ; base exp
+	db 45 ; catch rate
+	db 160 ; base exp
 
 	INCBIN "gfx/pokemon/front/seaking.pic", 0, 1 ; sprite dimensions
 	dw SeakingPicFront, SeakingPicBack
